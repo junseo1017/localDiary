1bcd46e23733b5d1de30fa99620065e5d70daf6a
leeJS <dlwnstjzld@gmail.com>
2023-10-14 18:49:14 +0900

    set login css & login function

--- src/component/LoginForm.tsx
+++ src/component/LoginForm.tsx
@@ -1,37 +1,51 @@
+/** @jsxImportSource @emotion/react */
+
 import React, { FC } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-
+import { css } from "@emotion/react";
+import { FormInput } from "../style/component/formInput";
+import { useNavigate } from "react-router-dom";
 interface LoginFormValue {
   id: string;
   pw: string;
 }
 
 const LoginForm: FC = () => {
+  const navigate = useNavigate();
   const { register, handleSubmit } = useForm<LoginFormValue>();
   const onLoginFormSubmitHandler: SubmitHandler<LoginFormValue> = async (
     data
   ) => {
     console.log(data);
   };
+  const onsignUpBtnClickHandler = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    navigate({ pathname: "/signup" });
+  };
+
   return (
-    <form onSubmit={handleSubmit(onLoginFormSubmitHandler)}>
-      <label htmlFor="id">ID</label>
-      <input
-        autoComplete="off"
-        type="text"
-        {...register("id", { required: true })}
-      />
-      <label htmlFor="pw">PASSWORD</label>
-      <input
-        autoComplete="off"
-        type="password"
-        {...register("pw", { required: true })}
-      />
-      <div>
-        <button>ログイン</button>
-        <button>新規登録</button>
-      </div>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit(onLoginFormSubmitHandler)}>
+        <label htmlFor="id">ID</label>
+        <input
+          css={FormInput}
+          autoComplete="off"
+          type="text"
+          {...register("id", { required: true })}
+        />
+        <label htmlFor="pw">PASSWORD</label>
+        <input
+          css={FormInput}
+          autoComplete="off"
+          type="password"
+          {...register("pw", { required: true })}
+        />
+        <div>
+          <button type="submit">ログイン</button>
+          <button onClick={onsignUpBtnClickHandler}>新規登録</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
